b26da88a2c7b729145994ada18801f6002f1e94c
MuhammadAsandyPutra <asandyputra77@gmail.com>
2026-06-18 17:00:11 +0800

    Move some components to components folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { About } from "@/src/app/about/page";
-import { Hero } from "@/src/app/hero/page";
+import { About } from "@/src/components/about/page";
+import { Hero } from "@/src/components/hero/page";
 import { SiteHeader } from "@/src/components/layout/site-header";
 import { SiteFooter } from "@/src/components/layout/site-footer";
-import { Work } from "./works/page";
-import { Skills } from "@/src/app/skills/page";
-import { Experience } from "./experience/page";
-import { Contact } from "./contact/page";
+import { Work } from "../components/works/page";
+import { Skills } from "@/src/components/skills/page";
+import { Experience } from "../components/experience/page";
+import { Contact } from "../components/contact/page";
 
 export default function Home() {
   return (
